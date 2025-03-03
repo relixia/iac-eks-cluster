4b4eafac52369a4a52ac14f142d9e27d82733865
relixia <bugra-net@hotmail.com>
2025-03-03 18:09:04 +0300

    Add GitLab CI/CD configuration and secrets.tf for fetching secrets

---
+++ secrets.tf
@@ -0,0 +1,12 @@
+# secrets.tf
+data "aws_secretsmanager_secret" "nottobeused" {
+  secret_id = "nottobeused"
+}
+
+data "aws_secretsmanager_secret_version" "nottobeused_version" {
+  secret_id = data.aws_secretsmanager_secret.nottobeused.id
+}
+
+output "studycase_secret_value" {
+  value = data.aws_secretsmanager_secret_version.nottobeused_version.secret_string
+}
